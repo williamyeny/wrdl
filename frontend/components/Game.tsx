8199a979e72da71c6b497ceda7c184d9f82323b5
Will Ye <williamyeny@gmail.com>
2022-01-16 04:02:55 -0500

    Fix dupe logic

--- frontend/components/Game.tsx
+++ frontend/components/Game.tsx
@@ -3,69 +3,87 @@ import { CharBox } from "./CharBox";
 import { Instructions } from "./Instructions";
 import { Keyboard } from "./Keyboard";
 
-const testWords = [
-  [
-    { value: "a", type: "wrong" },
-    { value: "a", type: "almost" },
-    { value: "a", type: "right" },
-    { value: "a", type: "wrong" },
-    { value: "a", type: "wrong" },
-    { value: "a", type: "wrong" },
-  ],
-  [
-    { value: "a", type: "wrong" },
-    { value: "a", type: "almost" },
-    { value: "a", type: "right" },
-    { value: "a", type: "wrong" },
-    { value: "a", type: "wrong" },
-    { value: "a", type: "wrong" },
-  ],
-];
+const WordRow = ({
+  guessWord,
+  solutionWord,
+  isInput = false,
+}: {
+  guessWord: string;
+  solutionWord: string;
+  isInput?: boolean; // Allows for incomplete word.
+}) => {
+  let colors: ("grey" | "green" | "white" | "yellow")[];
+  if (!isInput) {
+    colors = new Array(guessWord.length).fill("grey");
 
-type Word = {
-  value: string;
-  type: "empty" | "wrong" | "almost" | "right";
-}[];
+    // Need frequency array to handle char duplicates in guess/solution word.
+    // e.g., with solutionWord "ae" and guessWord "ee", the boxes should be "grey, green".
+    const freqs: { [key: string]: number } = {};
+    for (const char of solutionWord) {
+      if (!(char in freqs)) {
+        freqs[char] = 0;
+      }
+      freqs[char]++;
+    }
 
-const WordRow = ({ word }: { word: Word }) => (
-  <div className="flex gap-1 justify-start">
-    {word.map(({ value, type }, i) => (
-      <CharBox
-        key={i}
-        character={value}
-        color={
-          type === "empty"
-            ? "bg-white"
-            : type === "wrong"
-            ? "bg-stone-200"
-            : type === "right"
-            ? "bg-green-400"
-            : type === "almost"
-            ? "bg-yellow-300"
-            : ""
-        }
-      />
-    ))}
-    {new Array(5 - word.length).fill(0).map((_, i) => (
-      <CharBox key={i} character="" color="bg-white" />
-    ))}
-  </div>
-);
+    // Mark the right chars in right pos.
+    for (let i = 0; i < guessWord.length; i++) {
+      if (guessWord[i] === solutionWord[i]) {
+        freqs[guessWord[i]]--;
+        colors[i] = "green";
+      }
+    }
+
+    // Mark the right chars in wrong pos.
+    for (let i = 0; i < guessWord.length; i++) {
+      if (
+        guessWord[i] !== solutionWord[i] &&
+        guessWord[i] in freqs &&
+        freqs[guessWord[i]] > 0
+      ) {
+        freqs[guessWord[i]]--;
+        colors[i] = "yellow";
+      }
+    }
+  } else {
+    colors = new Array(guessWord.length).fill("white");
+  }
+
+  return (
+    <div className="flex gap-1 justify-start">
+      {guessWord.split("").map((char, i) => (
+        <CharBox
+          key={i}
+          character={char}
+          color={
+            colors[i] === "white"
+              ? "bg-white"
+              : colors[i] === "green"
+              ? "bg-green-400"
+              : colors[i] === "yellow"
+              ? "bg-yellow-300"
+              : colors[i] === "grey"
+              ? "bg-stone-200"
+              : ""
+          }
+        />
+      ))}
+      {/* If incomplete word, fill rest with empty boxes. */}
+      {new Array(5 - guessWord.length).fill(0).map((_, i) => (
+        <CharBox key={i} character="" color="bg-white" />
+      ))}
+    </div>
+  );
+};
 
 export const Game = () => {
-  const [guesses, setGuesses] = useState<Word[]>([]);
-  const [currentGuess, setCurrentGuess] = useState<Word>([]);
+  const [guesses, setGuesses] = useState<string[]>([]);
+  const [currentGuess, setCurrentGuess] = useState<string>("");
 
   const addCharacter = useCallback(
     (character: string) => {
       if (currentGuess.length < 5) {
-        setCurrentGuess([
-          ...currentGuess,
-          {
-            value: character,
-            type: "empty",
-          },
-        ]);
+        setCurrentGuess(currentGuess + character);
       }
     },
     [currentGuess]
@@ -74,7 +92,7 @@ export const Game = () => {
   const onSubmit = useCallback(() => {
     if (currentGuess.length === 5) {
       setGuesses([...guesses, currentGuess]);
-      setCurrentGuess([]);
+      setCurrentGuess("");
     }
   }, [currentGuess, guesses]);
 
@@ -110,9 +128,9 @@ export const Game = () => {
     <div>
       <div className="flex flex-col gap-1 mt-1 w-[calc(320px+1rem)] mx-auto">
         {guesses.map((guess, i) => (
-          <WordRow word={guess} key={i} />
+          <WordRow guessWord={guess} key={i} solutionWord={"eaeee"} />
         ))}
-        <WordRow word={currentGuess} />
+        <WordRow guessWord={currentGuess} solutionWord={"eaeee"} isInput />
       </div>
       {guesses.length === 0 && <Instructions />}
       <Keyboard
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
