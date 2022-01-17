4f9f086689adc39736d34ada79a1f0cd7f1a9442
Will Ye <williamyeny@gmail.com>
2022-01-17 15:41:22 -0500

    Switch to netlify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix:
-    process.env.NODE_ENV === "development" ? "/" : "/word-guesser/out/",
 };
 
 module.exports = nextConfig;
--- out/_next/static/IS3mW_AAvZJmn6Hle7mHc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/603-730b3cd0a83ae85b.js","static/chunks/pages/index-be20c8b199ccc5a8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
