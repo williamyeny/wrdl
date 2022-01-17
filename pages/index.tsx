d0b03d02a0c4a40673e3e22da5092781b2d27840
Will Ye <williamyeny@gmail.com>
2022-01-16 19:26:32 -0500

    Change title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Word Guesser</title>
         <meta name="description" content="Word Guesser game" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
